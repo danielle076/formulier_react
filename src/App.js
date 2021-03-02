2718031fe85a60b5eb76afa9896671c12540c88a
danielle076 <d.vandenakker@novi-education.nl>
2021-03-02 12:36:12 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,143 @@
+import { useForm } from 'react-hook-form';
 import './App.css';
 
-function App() {
-  return (
-      <div>
-        Hallo allemaal
+// Statisch formulier
+// function App() {
+//     function onFormSubmit(e) {
+//         e.preventDefault();
+//         console.log('Submitted!');
+//     }
+//
+//     return (
+//         <form onSubmit={onFormSubmit}>
+//             <fieldset>
+//                 <legend>Gegevens</legend>
+//
+//                 <label htmlFor="details-name">
+//                     Naam:
+//                     <input
+//                         type="text"
+//                         name="name"
+//                         id="details-name"
+//                     />
+//                 </label>
+//
+//                 <label htmlFor="details-age">
+//                     Leeftijd:
+//                     <input
+//                         type="number"
+//                         name="age"
+//                         id="details-age"
+//                     />
+//                 </label>
+//             </fieldset>
+//
+//             <fieldset>
+//                 <legend>Jouw review</legend>
+//
+//                 <label htmlFor="referrer">
+//                     Hoe heb je dit recept gevonden?
+//                     <select
+//                         name="found-through"
+//                         id="referrer"
+//                     >
+//                         <option value="google">Google</option>
+//                         <option value="vriend">Vriend</option>
+//                         <option value="advertentie">Advertentie</option>
+//                         <option value="anders">Anders</option>
+//                     </select>
+//                 </label>
+//
+//                 <label htmlFor="recipe-comments">
+//                     Opmerkingen:
+//                     <textarea
+//                         name="comments"
+//                         id="recipe-comments"
+//                         rows="4"
+//                         cols="40"
+//                         placeholder="Wat vond je van het recept?"
+//                     >
+//           </textarea>
+//                 </label>
+//
+//                 <button type="submit">
+//                     Versturen
+//                 </button>
+//             </fieldset>
+//         </form>
+//     );
+// }
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, assumenda blanditiis consequatur, ducimus ea enim harum illo impedit iusto maiores, maxime minus nesciunt odio quia repellat similique totam unde vero.</p>
-      </div>
-  );
+// Formulier met react-hook-form
+function App() {
+    const { handleSubmit, register } = useForm();
+
+    function onFormSubmit(data) {
+         console.log(data);
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onFormSubmit)}>
+            <fieldset>
+                <legend>Gegevens</legend>
+
+                <label htmlFor="details-name">
+                    Naam:
+                    <input
+                        type="text"
+                        name="name"
+                        id="details-name"
+                        ref={register}
+                    />
+                </label>
+
+                <label htmlFor="details-age">
+                    Leeftijd:
+                    <input
+                        type="number"
+                        name="age"
+                        id="details-age"
+                        ref={register}
+                    />
+                </label>
+            </fieldset>
+
+            <fieldset>
+                <legend>Jouw review</legend>
+
+                <label htmlFor="referrer">
+                    Hoe heb je dit recept gevonden?
+                    <select
+                        name="found-through"
+                        id="referrer"
+                        ref={register}
+                    >
+                        <option value="google">Google</option>
+                        <option value="vriend">Vriend</option>
+                        <option value="advertentie">Advertentie</option>
+                        <option value="anders">Anders</option>
+                    </select>
+                </label>
+
+                <label htmlFor="recipe-comments">
+                    Opmerkingen:
+                    <textarea
+                        name="comments"
+                        id="recipe-comments"
+                        rows="4"
+                        cols="40"
+                        placeholder="Wat vond je van het recept?"
+                        ref={register}
+                    >
+          </textarea>
+                </label>
+
+                <button type="submit">
+                    Versturen
+                </button>
+            </fieldset>
+        </form>
+    );
 }
 
 export default App;
