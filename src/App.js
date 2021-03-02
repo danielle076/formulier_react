3ae928e8197b7d56a35fb518d75c89f1c08c6c7e
danielle076 <d.vandenakker@novi-education.nl>
2021-03-02 10:59:18 +0100

    start project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        Hallo allemaal
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, assumenda blanditiis consequatur, ducimus ea enim harum illo impedit iusto maiores, maxime minus nesciunt odio quia repellat similique totam unde vero.</p>
+      </div>
+  );
+}
+
+export default App;
